c21dde0bfe30efb0f5f4ce34d9af44f1289cd80b
Sushang Agnihotri <53427450+Sushang-Agnihotri@users.noreply.github.com>
2021-07-22 16:17:17 +0530

    Update urls.py

--- MultipleImgUploadPro/urls.py
+++ MultipleImgUploadPro/urls.py
@@ -18,7 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-#    path('api/',include('uploadimg.urls')),
     path('api/',include('uploadImg.urls'))
-
 ]
